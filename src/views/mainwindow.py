fe7101684ba40cc16e5c6fafa3f7614f03a56072
macbook <dinhbinhnguyen.main@gmail.com>
2025-04-28 13:04:43 +0700

    from macbook

--- src/views/mainwindow.py
+++ src/views/mainwindow.py
@@ -1,6 +1,7 @@
 from typing import List
-from PyQt6.QtWidgets import QMainWindow
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QMainWindow, QDialogButtonBox
+from PyQt6.QtCore import Qt, QRegularExpression
+from PyQt6.QtGui import QRegularExpressionValidator
 from src.ui.mainwindow_ui import Ui_MainWindow
 
 from src.views.thread_container_w import ThreadContainer_Widget
@@ -21,6 +22,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def setup_ui(self):
         self.thread_num_input.setValue(0)
 
+        regex = QRegularExpression("^[a-zA-Z0-9 &,]+$")
+        validator = QRegularExpressionValidator(regex)
+        self.group_key_input.setValidator(validator)
+        self.ignore_group_key_input.setValidator(validator)
+
+        self.ok_btn = self.buttonBox.button(QDialogButtonBox.StandardButton.Ok)
+        self.ok_btn.clicked.connect(self.handle_run)
+
     def setup_events(self):
         self.thread_num_input.valueChanged.connect(self.handle_change_thread)
 
@@ -28,19 +37,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         current_thread_num = self.thread_num_input.value()
         spread = current_thread_num - self.thread_num
         if spread > 0:  # add thread
-            while spread == 0:
+            while spread != 0:
                 thread_container_widget = ThreadContainer_Widget(self)
-                self.thread_container_layout.addWiget(thread_container_widget)
-                # thread_container_widget.set
+                self.threads_container_layout.addWidget(thread_container_widget)
                 self.list_thread_widget.append(thread_container_widget)
                 spread -= 1
         elif spread < 0:  # remove thread
-            while spread == 0:
+            while spread != 0:
                 last_thread_widget = self.list_thread_widget.pop()
+                self.threads_container_layout.removeWidget(last_thread_widget)
                 last_thread_widget.deleteLater()
+                spread += 1
         else:
             pass
+        self.thread_num = current_thread_num
 
-        self.list_thread_widget.append()
+    def handle_run(self):
+        list_udd = [
+            thread_container_widget.selected_dir_path
+            for thread_container_widget in self.list_thread_widget
+        ]
+        target_group_keywords = [
+            keyword.strip() for keyword in self.group_key_input.text().split(",")
+        ]
+        ignore_group_keywords = [
+            keyword.strip() for keyword in self.ignore_group_key_input.text().split(",")
+        ]
 
-        self.thread_num = self.thread_num_input.value()
+        print(list_udd)
+        print(target_group_keywords)
+        print(ignore_group_keywords)
+
+        # TODO start robot controller
--- src/views/thread_container_w.py
+++ src/views/thread_container_w.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QFileDialog
 
 from src.ui.thread_container_ui import Ui_ThreadContainer
 
@@ -7,3 +7,23 @@ class ThreadContainer_Widget(QWidget, Ui_ThreadContainer):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.selected_dir_path = ""
+        self.setup_ui()
+        self.setup_events()
+
+    def setup_ui(self):
+        self.thread_input.setDisabled(True)
+
+    def setup_events(self):
+        self.pushButton.clicked.connect(self.handle_open_directory)
+
+    def handle_open_directory(self):
+        directory_path = QFileDialog.getExistingDirectory(
+            self,
+            "Select user_data_dir",
+            "",
+            QFileDialog.Option.ShowDirsOnly,
+        )
+        if directory_path:
+            self.thread_input.setText(directory_path)
+            self.selected_dir_path = directory_path
